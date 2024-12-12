d5ede03f797712b265507556ca88461e9805bcf3
LibraryMatt <139739544+LibraryMatt@users.noreply.github.com>
2024-12-12 11:10:59 +1100

    Update DLSCHTASK.ps1

--- DLSCHTASK.ps1
+++ DLSCHTASK.ps1
@@ -1,6 +1,9 @@
 # Define variables
 $repoUrl = "https://github.com/LibraryMatt/BackgroundSetup/blob/main/schtask.ps1?raw=true"
 $destinationPath = "C:\mbm\schtask.ps1"
+$repoUrl1 = "https://github.com/LibraryMatt/BackgroundSetup/blob/main/setBackground.ps1?raw=true"
+$destinationPath1 = "C:\mbm\setBackground.ps1"
+
 
 # Create the destination directory if it doesn't exist
 $destinationDir = Split-Path -Path $destinationPath -Parent
@@ -8,8 +11,18 @@ if (-not (Test-Path -Path $destinationDir)) {
     New-Item -ItemType Directory -Path $destinationDir -Force
 }
 
+# Create the destination directory if it doesn't exist
+$destinationDir = Split-Path -Path $destinationPath1 -Parent
+if (-not (Test-Path -Path $destinationDir)) {
+    New-Item -ItemType Directory -Path $destinationDir -Force
+}
+
 # Download the file
 Invoke-WebRequest -Uri $repoUrl -OutFile $destinationPath
+
+# Download the file
+Invoke-WebRequest -Uri $repoUrl1 -OutFile $destinationPath1
+
 
 # Confirm download
 if (Test-Path -Path $destinationPath) {
